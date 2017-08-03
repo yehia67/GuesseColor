66cca775a12796d1dae9c56788ddc80335a272c2
yehia67 <yehiatarek67@gmail.com>
2017-08-03 03:36:51 +0200

    finish design and start coding

---
+++ app/src/main/java/com/example/yehia/guessthecolor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yehia.guessthecolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+   int score;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
